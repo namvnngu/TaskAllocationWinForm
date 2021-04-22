2c1c8ef82b8265a761d8d883087cbb0598a2cc3b
Nam Nguyen <nguyenvietnam2401@gmail.com>
2021-04-22 14:11:10 +1000

    fix: refactor AlloationsDisplay

--- TasksAllocation/Utils/Display/AllocationsDisplay.cs
+++ TasksAllocation/Utils/Display/AllocationsDisplay.cs
@@ -19,7 +19,7 @@ namespace TasksAllocation.Utils.Display
                 int allocationID = allocationDisplay.ID;
                 double allocationRuntime = allocationDisplay.Runtime;
                 double allocationEnergy = allocationDisplay.Energy;
-                List<ProcessorAllocation> prococessAllocations = allocationDisplay.ProcessorAllocations;
+                List<ProcessorAllocation> processorAllocations = allocationDisplay.ProcessorAllocations;
 
                 // Allocation Info
                 renderedText.Append("<br><table>");
@@ -35,28 +35,37 @@ namespace TasksAllocation.Utils.Display
                 renderedText.Append($"<th style=\"text-align:left\">Upload</th>");
                 renderedText.Append("</tr>");
 
-                for (int processorAllocationNum = 0; processorAllocationNum < prococessAllocations.Count; processorAllocationNum++)
-                {
-                    ProcessorAllocation processorAllocation = prococessAllocations[processorAllocationNum];
-                    string processTaskAllocation = processorAllocation.Allocation;
-                    int processTaskAllocationRAM = processorAllocation.RAM;
-                    int processTaskAllocationDownload = processorAllocation.Download;
-                    int processTaskAllocationUpload = processorAllocation.Upload;
-                    Processor processor = configuration.Processors[processorAllocationNum];
-                    int processRAM = processor.RAM;
-                    int processDownload = processor.Download;
-                    int processUpload = processor.Upload;
-
-                    // Processor Allocation Info
-                    renderedText.Append("<tr>");
-                    renderedText.Append($"<td>{processTaskAllocation}</td>");
-                    renderedText.Append($"<td>{processTaskAllocationRAM}/{processRAM} GB</td>");
-                    renderedText.Append($"<td>{processTaskAllocationDownload}/{processDownload} Gbps</td>");
-                    renderedText.Append($"<td>{processTaskAllocationUpload}/{processUpload} Gbps</td>");
-                    renderedText.Append("</tr>");
-                }
+                // Body
+                renderedText.Append(DisplayProcessorAllocation(processorAllocations, configuration));
 
                 renderedText.Append("</table>");
+            }
+
+            return renderedText.ToString();
+        }
+
+        private static string DisplayProcessorAllocation(List<ProcessorAllocation> processorAllocations, Configuration configuration)
+        {
+            StringBuilder renderedText = new StringBuilder();
+
+            for (int processorAllocationNum = 0; processorAllocationNum < processorAllocations.Count; processorAllocationNum++)
+            {
+                ProcessorAllocation processorAllocation = processorAllocations[processorAllocationNum];
+                string processTaskAllocation = processorAllocation.Allocation;
+                int processTaskAllocationRAM = processorAllocation.RAM;
+                int processTaskAllocationDownload = processorAllocation.Download;
+                int processTaskAllocationUpload = processorAllocation.Upload;
+                Processor processor = configuration.Processors[processorAllocationNum];
+                int processRAM = processor.RAM;
+                int processDownload = processor.Download;
+                int processUpload = processor.Upload;
+
+                renderedText.Append("<tr>");
+                renderedText.Append($"<td>{processTaskAllocation}</td>");
+                renderedText.Append($"<td>{processTaskAllocationRAM}/{processRAM} GB</td>");
+                renderedText.Append($"<td>{processTaskAllocationDownload}/{processDownload} Gbps</td>");
+                renderedText.Append($"<td>{processTaskAllocationUpload}/{processUpload} Gbps</td>");
+                renderedText.Append("</tr>");
             }
 
             return renderedText.ToString();
